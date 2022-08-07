19c21bdefd525349ac1578301bb5635aec993707
svasin <bloodfall98.130@gmail.com>
2022-08-08 00:00:19 +0300

    + get_response_if_cats improvement

--- src/response_module.py
+++ src/response_module.py
@@ -64,8 +64,29 @@ def get_response_if_bored(msg, group_id):
 
 
 def get_response_if_cats(vk, msg, group_id):
+    codes = [
+        '100', '101', '102',
+
+        '200', '201', '202', '203', '204', '206', '207',
+
+        '300', '301', '302', '303', '304', '305', '307', '308',
+
+        '400', '401', '402', '403', '404', '405', '406', '407', '408',
+        '409', '410', '411', '412', '413', '414', '415', '416', '417',
+        '418', '420', '421', '422', '423', '424', '425', '426', '429',
+        '431', '444', '450', '451', '497', '498', '499',
+
+        '500', '501', '502', '503', '504', '506', '507', '508',
+        '509', '510', '511', '521', '522', '523', '525', '599'
+    ]
+
     bot_summoning = f'(\[club{group_id}\|[@]?[a-zA-Z_0-9]+\][,]?|Y|y)'
-    if re.match(f'{bot_summoning} g c.*', msg):
+    if re.fullmatch(f'{bot_summoning} g c', msg):
+        code = rand_module.get_random_iterable_item(codes)
+        return a_module.get_http_cats(vk, code)
+    elif re.match(f'{bot_summoning} g c.*', msg):
         code = re.search('c .*', msg)
-        code = '401' if code is None else code.group(0)[2:]
+        code = code.group(0)[2:] \
+            if code is not None and code.group(0)[2:] in codes \
+            else '404'
         return a_module.get_http_cats(vk, code)
